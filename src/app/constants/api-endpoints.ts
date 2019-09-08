b03880a4e92fe32ecd1fec0b78c721e651f03d68
gautam mishra <gautamm@mindfiresolutions.com>
2019-09-08 17:23:56 +0530

    change in api constant

--- src/app/constants/api-endpoints.ts
+++ src/app/constants/api-endpoints.ts
@@ -2,7 +2,7 @@ import { environment } from 'src/environments/environment';
 
 const baseUrl  = environment.baseUrl;
 export const ApiEndPoints =  {
-    registerUser: `${baseUrl}/user/register`,
+    registerUser: `${baseUrl}/register`,
     login: `http://localhost:8080/login`,
     userInfo: `${baseUrl}/user/currentuser`,
 };
